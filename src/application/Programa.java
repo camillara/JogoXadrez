ccf9a3f75329adbf011fafb8b2130d4e6ed739a0
Camilla Rodrigues <96442004+camillara@users.noreply.github.com>
2022-04-21 11:10:41 -0300

    ClassePosicao
    Classe posição no tabuleiro foi criada.

---
+++ src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {		
+		Posicao pos = new Posicao(3,5);		
+		System.out.println(pos);
+
+	}
+
+}
